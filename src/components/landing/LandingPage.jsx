7889b7a9d22cb4137f8eb73ca3a251fe21d2580e
Franco Bondioni <bondionifranco@gmailcom>
2024-01-10 01:25:51 -0300

    change landing

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,13 +1,15 @@
-import { useLocation } from 'react-router';
+import { useEffect, useState } from 'react'; 
 import { CarteleraPelicula } from "../movie/CarteleraPelicula";
-export function LandingPage() {
+export function LandingPage() { const [search, setSearch] = useState("");
+useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const searchParam = queryParams.get("search");
+    setSearch(searchParam || "");
+}, []);
 
-    const query =  new URLSearchParams(useLocation().search);
-    const search = query.get("search");
-
-    return (
-        <>
-            <CarteleraPelicula key={search} search={search}/>
-        </>
-    );
+return (
+    <>
+        <CarteleraPelicula key={search} search={search}/>
+    </>
+);
 }
