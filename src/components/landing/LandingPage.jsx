32fcd809dae476d3dd76b0c36e601fe96e3d4d62
Franco Bondioni <bondionifranco@gmailcom>
2024-01-10 01:50:29 -0300

    code change due to search error

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react'; 
+import { useLocation } from 'react-router';
 import { CarteleraPelicula } from "../movie/CarteleraPelicula";
-export function LandingPage() { const [search, setSearch] = useState("");
-useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const searchParam = queryParams.get("search");
-    setSearch(searchParam || "");
-}, []);
+export function LandingPage() {
 
-return (
-    <>
-        <CarteleraPelicula key={search} search={search}/>
-    </>
-);
+    const query =  new URLSearchParams(useLocation().search);
+    const search = query.get("search");
+
+    return (
+        <>
+            <CarteleraPelicula key={search} search={search}/>
+        </>
+    );
 }
